f510c053bb25be2b4474fe3accf6d8092915937f
Wubert <242257@edu.p.lodz.pl>
2023-10-22 04:06:10 +0200

    csv reading

--- Sudoku_game/Sudoku.py
+++ Sudoku_game/Sudoku.py
@@ -1,1 +1,19 @@
-print("Hello dear user!\nHere is sudoku game you can play\n\nGood luck!")
+import csv 
+
+def choosing_csv(filename, user_choice):
+
+    with open(filename) as sudokus:
+        csv_reader = csv.reader(sudokus)
+        rows = list(csv_reader)
+        templist = list(rows[user_choice])
+        puzzle = templist[0]
+       
+        return str(puzzle)
+
+
+print("Hello, welcome to sudoku game")
+
+user_choice = int(input("Choose number from 1 to 20 to select your puzzle: "))
+sudoku = list(choosing_csv("sudoku.csv", user_choice))
+print("This is your puzzle\n", sudoku)
+
